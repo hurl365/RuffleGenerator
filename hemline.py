122d6d52615e07ba90a72523039a904d974e5f13
Ruilin Hu <ruhu@ucsd.edu>
2023-08-31 17:09:06 -0700

    Create hemline.py

---
+++ hemline.py
@@ -0,0 +1,11 @@
+'''Generate the bottom hemline of the dress.
+
+The functions and code here are used to generate the bottom hemline 
+of the dress, which will be formed by connecting the points using
+their x and y coordinate on a 2D plane.
+
+Typical usage example:
+
+hemlineGenerared = generateHemline(pairOfFolds, minFoldRadius, maxFoldRadius,
+                                    maxHeight, maxWidth)
+'''
