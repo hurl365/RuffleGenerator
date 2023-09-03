eee920f568d9ba03bfb5784ee5b86a29cfdb3ccd
Ruilin Hu <ruhu@ucsd.edu>
2023-09-03 16:17:09 -0700

    Upload Radii Generation Code

--- hemline.py
+++ hemline.py
@@ -7,13 +7,13 @@ their x and y coordinate on a 2D plane.
 Typical usage example:
 
 hemlineGenerared = generateHemline(pairOfFolds, minFoldRadius, maxFoldRadius,
-                                    maxHeight, maxWidth, numCurvePoints)
+                                    numCurvePoints)
 '''
 # necessary imports
-import random
+import random, math
 
 def generateHemline(pairOfFolds, minFoldRadius, maxFoldRadius,
-                    maxHeight, maxWidth, numCurvePoints) -> list:
+                    numCurvePoints) -> list:
     '''generate the points to form the hemline (without width/thickness)
 
     Using the given input to create points to form the hemline that does not
@@ -29,12 +29,6 @@ def generateHemline(pairOfFolds, minFoldRadius, maxFoldRadius,
         maxFoldRadius:
             The maximum value of the radius for each fold
             Must be a positive number. 0 if there is no limit.
-        maxHeight:
-            The maximum y-coordinate value for the points generated
-            Must be a positive number. 0 if there is no limit.
-        maxWidth:
-            The maximum x-coordinate value for the points generated
-            Must be a positive number. 0 if there is no limit.
         numCurvePoints:
             The number of point to generate for each fold (circle segment)
             Minimum is 2, must be a positive integer.
@@ -49,18 +43,65 @@ def generateHemline(pairOfFolds, minFoldRadius, maxFoldRadius,
             represents the coordinate of a point.
     '''
     hemlineGenerated = []
-    circleCenters = generateCircleCenters(pairOfFolds, minFoldRadius, maxFoldRadius,
-                                          maxHeight, maxWidth)
+    circleRadii = generateCircleRadii(pairOfFolds, minFoldRadius, maxFoldRadius)
+    circleCenters = generateCircleCenters(pairOfFolds, circleRadii)
 
-def generateCircleCenters(pairOfFolds, minFoldRadius, maxFoldRadius,
-                                          maxHeight, maxWidth) -> list:
-    prevY = random.uniform(0, maxHeight) # find a random starting point
+def generateCircleRadii(pairOfFolds, minFoldRadius, maxFoldRadius) -> list:
+    # Adjust the values if necessary, make sure input is appropriate
+    if maxFoldRadius == 0: # if there is no upper bound for fold radius
+        maxFoldRadius = float('inf') # the upper bound if inifinity
+    if minFoldRadius < 0: # make sure the lower bound is not negative
+        minFoldRadius = 0
+    circleRadii = [] # the list of circle radii
+    for centerCount in range(0, pairOfFolds * 2):
+        # Randomly generate the next radius in the given range and append to list
+        nextCircleRadius = random.uniform(minFoldRadius, maxFoldRadius)
+        circleRadii.append(nextCircleRadius)
+    return circleRadii
+
+def generateCircleCenters(pairOfFolds, circleRadii) -> list:
+    circleCenters = [] # store the generated circle centers
+    # Find the first circle's center
+    prevRadius = circleRadii[0]
+    prevY = random.uniform(0, prevRadius * 2) # find a random starting point
     prevX = 0 # starting from x = 0 (leftmost)
-    for centerCount in range [0, pairOfFolds * 2 - 1]:
-        [deltaX, deltaY] = getDeltaToNextCircleCenter(prevX, prevY, minFoldRadius, 
-                                                      maxFoldRadius, maxHeight, maxWidth)
+    # Use trig to calculate the change in x and y coordinate
+    [deltaX, deltaY] = getDeltaToNextCircleCenter(prevRadius)
+    currX = prevX + deltaX # go to the right of the origin for the first point
+    currY = prevY + deltaY
+    prevX = currX # record the previously calculated circle center coordinate
+    prevY = currY
+    circleCenters.append((currX, currY)) # Append the tuple (coordinate) to the list
+    for centerCount in range(1, pairOfFolds * 2):
+        # Amount of shift = radius for the previous fold + radius for current fold
+        currRadius = circleRadii[centerCount]
+        deltaRadius = prevRadius + currRadius
+        # Use trig to calculate the change in x and y coordinate
+        [deltaX, deltaY] = getDeltaToNextCircleCenter(deltaRadius)
         currX = prevX + deltaX # keep going right for the next point
         currY = prevY + deltaY
         prevX = currX
         prevY = currY
+        prevRadius = currRadius
+        circleCenters.append((currX, currY)) # Append the tuple to the list
+    return circleCenters
 
+def getDeltaToNextCircleCenter(radius) -> list:
+    # randomly generate the angle of projection to the next point
+    projectAngle = random.uniform(-math.pi / 2, math.pi / 2)
+    # find the change in x and y changes to get to the next point
+    deltaX = radius * math.cos(projectAngle)
+    deltaY = radius * math.sin(projectAngle)
+    return [deltaX, deltaY]
+
+def sanityTest():
+    print("******Executing sanity test: getDeltaToNextCircleCenter******")
+    print(getDeltaToNextCircleCenter(5))
+    print("******Executing sanity test: generateCircleRadii******")
+    print(generateCircleRadii(2, 1, 9))
+    print("******Executing sanity test: generateCircleCenters******")
+    print(generateCircleCenters(2, [1, 9, 3, 7]))
+
+if __name__ == '__main__':
+    print("******Executing sanity tests******")
+    sanityTest()
